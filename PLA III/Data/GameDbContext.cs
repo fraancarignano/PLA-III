51f89f6e9d422f9b89c4625b08e7afd074857308
Octa Rodriguez <totorodriguez322+1@gmail.com>
2025-10-23 14:24:43 -0300

    DbContext

---
+++ PLA III/Data/GameDbContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using PLA_III.Models;
+
+
+namespace PLA_III.Data
+{
+    public class GameDbContext : DbContext
+    {
+        public GameDbContext(DbContextOptions<GameDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+
+        public DbSet<Game> Games { get; set; }
+
+        public DbSet<Attempt> Attempts { get; set; }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<Player>()
+                .HasKey(p => p.PlayerId); 
+
+            
+            modelBuilder.Entity<Game>()
+                .HasKey(g => g.GameId); 
+
+            
+            modelBuilder.Entity<Attempt>()
+                .HasKey(a => a.AttemptId); 
+
+            
+
+            
+            modelBuilder.Entity<Game>()
+                .HasOne(g => g.Player)          
+                .WithMany(p => p.Games)         
+                .HasForeignKey(g => g.Player) 
+                .IsRequired();                  
+
+            
+            modelBuilder.Entity<Attempt>()
+                .HasOne(a => a.Game)            
+                .WithMany(g => g.Attempts)      
+                .HasForeignKey(a => a.Game)   
+                .IsRequired();                  
+
+           
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
